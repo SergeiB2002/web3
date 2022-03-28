581c2406a501ca80e0c4072553609a4efefaeddf
Sergei Brezitskii <90458734+SergeiB2002@users.noreply.github.com>
2022-03-28 17:39:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,39 @@ $pol = $_POST['radio-pol'];
 $limbs = intval($_POST['radio-limb']);
 $superpowers = $_POST['field-super'];
 $bio= $_POST['field-bio'];
+
+$bioreg = "/^\s*\w+[\w\s\.,-]*$/";
+$reg = "/^\w+[\w\s-]*$/";
+$mailreg = "/^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/";
+$list_sup = array('immortal','noclip','power','telepat');
+
+if(!preg_match($reg,$name)){
+	print_r('Неверный формат имени');
+	exit();
+}
+if($limbs < 1){
+	print_r('Invalid am of limbs');
+	exit();
+}
+if(!preg_match($bioreg,$bio)){
+	print_r('Неверный формат биографии');
+	exit();
+}
+if(!preg_match($mailregex,$email)){
+	print_r('Неверный формат email');
+	exit();
+}
+if($pol !== 'man' && $pol !== 'woman'){
+	print_r('Неверный формат пола');
+	exit();
+}
+foreach($superpowers as $checking){
+	if(array_search($checking,$list_sup)=== false){
+			print_r('Неверный формат суперсил');
+			exit();
+	}
+}
+
 $user = 'u41028';
 $pass = '2356452';
 $db = new PDO('mysql:host=localhost;dbname=u41028', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
